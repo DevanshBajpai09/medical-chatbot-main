88b38c61a132b893b117b91c5515c3836672c63a
DevanshBajpai09 <devanshbajpai09@gmail.com>
2025-03-27 10:00:21 +0530

    first commit

---
+++ chat/app.py
@@ -0,0 +1,356 @@
+import os
+import streamlit as st
+from streamlit_option_menu import option_menu
+from dotenv import load_dotenv
+
+
+
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+load_dotenv()
+
+# For Chatbot
+from langchain.llms import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# For Heart Disease Prediction
+import pickle
+
+# Load environment variables (if using .env file)
+# from dotenv import load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())
+
+# Set up Streamlit page configuration
+st.set_page_config(page_title="Multiple Disease Prediction & Chatbot", layout="wide", page_icon="👨‍⚕️")
+
+# Load Heart Disease Model
+working_dir = os.path.dirname(os.path.abspath(__file__))
+diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes.pkl', 'rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart.pkl', 'rb'))
+kidney_disease_model = pickle.load(open(f'{working_dir}/saved_models/kidney.pkl', 'rb'))
+
+NewBMI_Overweight = 0
+NewBMI_Underweight = 0
+NewBMI_Obesity_1 = 0
+NewBMI_Obesity_2 = 0 
+NewBMI_Obesity_3 = 0
+NewInsulinScore_Normal = 0 
+NewGlucose_Low = 0
+NewGlucose_Normal = 0 
+NewGlucose_Overweight = 0
+NewGlucose_Secret = 0
+
+# Load Chatbot Components
+DB_FAISS_PATH = "vectorstore/db_faiss"
+HF_TOKEN = os.environ.get("CHAT_BOT_TOKEN")
+HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
+
+# Load LLM for Chatbot
+def load_llm(huggingface_repo_id):
+    llm = HuggingFaceEndpoint(
+        repo_id=huggingface_repo_id,
+        temperature=0.5,
+        model_kwargs={"token": HF_TOKEN, "max_length": "512"}
+    )
+    return llm
+
+# Custom Prompt Template for Chatbot
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer the user's question.
+If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+Don't provide anything outside of the given context.
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk, please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+# Load Vector Store for Chatbot
+@st.cache_resource
+def get_vectorstore():
+    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+    return db
+
+# Create QA Chain for Chatbot
+def create_qa_chain():
+    vectorstore = get_vectorstore()
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=load_llm(HUGGINGFACE_REPO_ID),
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever(search_kwargs={'k': 3}),
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+    )
+    return qa_chain
+
+# Sidebar for Navigation
+with st.sidebar:
+    selected = option_menu(
+        "Multiple Disease Prediction & Chatbot",
+        ['Heart Disease Prediction', 'Diabetes Prediction',
+                 
+                 'Kidney Disease Prediction','Chatbot'],
+        menu_icon='hospital-fill',
+        icons=['activity', 'heart', 'person', 'chat'],
+        default_index=0
+    )
+
+
+
+if selected == 'Diabetes Prediction':
+    st.title("Diabetes Prediction Using Machine Learning")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input("Number of Pregnancies")
+    with col2:
+        Glucose = st.text_input("Glucose Level")
+    with col3:
+        BloodPressure = st.text_input("BloodPressure Value")
+    with col1:
+        SkinThickness = st.text_input("SkinThickness Value")
+    with col2:
+        Insulin = st.text_input("Insulin Value")
+    with col3:
+        BMI = st.text_input("BMI Value")
+    with col1:
+        DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction Value")
+    with col2:
+        Age = st.text_input("Age")
+    diabetes_result = ""
+    if st.button("Diabetes Test Result"):
+        if float(BMI) <= 18.5:
+            NewBMI_Underweight = 1
+        elif 18.5 < float(BMI) <= 24.9:
+            pass
+        elif 24.9 < float(BMI) <= 29.9:
+            NewBMI_Overweight = 1
+        elif 29.9 < float(BMI) <= 34.9:
+            NewBMI_Obesity_1 = 1
+        elif 34.9 < float(BMI) <= 39.9:
+            NewBMI_Obesity_2 = 1
+        elif float(BMI) > 39.9:
+            NewBMI_Obesity_3 = 1
+        
+        if 16 <= float(Insulin) <= 166:
+            NewInsulinScore_Normal = 1
+
+        if float(Glucose) <= 70:
+            NewGlucose_Low = 1
+        elif 70 < float(Glucose) <= 99:
+            NewGlucose_Normal = 1
+        elif 99 < float(Glucose) <= 126:
+            NewGlucose_Overweight = 1
+        elif float(Glucose) > 126:
+            NewGlucose_Secret = 1
+
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                      BMI, DiabetesPedigreeFunction, Age, NewBMI_Underweight,
+                      NewBMI_Overweight, NewBMI_Obesity_1,
+                      NewBMI_Obesity_2, NewBMI_Obesity_3, NewInsulinScore_Normal, 
+                      NewGlucose_Low, NewGlucose_Normal, NewGlucose_Overweight,
+                      NewGlucose_Secret]
+        
+        user_input = [float(x) for x in user_input]
+        prediction = diabetes_model.predict([user_input])
+        if prediction[0] == 1:
+            diabetes_result = "The person has diabetes"
+        else:
+            diabetes_result = "The person does not have diabetes"
+    st.success(diabetes_result)
+
+# Repeat similar steps for Heart Disease and Kidney Disease Prediction
+
+if selected == 'Heart Disease Prediction':
+    st.title("Heart Disease Prediction Using Machine Learning")
+    col1, col2, col3  = st.columns(3)
+
+    with col1:
+        age = st.text_input("Age")
+    with col2:
+        sex = st.text_input("Sex")
+    with col3:
+        cp = st.text_input("Chest Pain Types")
+    with col1:
+        trestbps = st.text_input("Resting Blood Pressure")
+    with col2:
+        chol = st.text_input("Serum Cholestroal in mg/dl")
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    heart_disease_result = ""
+    if st.button("Heart Disease Test Result"):
+        user_input = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+        user_input = [float(x) for x in user_input]
+        prediction = heart_disease_model.predict([user_input])
+        if prediction[0]==1:
+            heart_disease_result = "This person is having heart disease"
+        else:
+            heart_disease_result = "This person does not have any heart disease"
+    st.success(heart_disease_result)
+
+if selected == 'Kidney Disease Prediction':
+    
+    st.title("Kidney Disease Prediction using ML")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        age = st.text_input('Age')
+
+    with col2:
+        blood_pressure = st.text_input('Blood Pressure')
+
+    with col3:
+        specific_gravity = st.text_input('Specific Gravity')
+
+    with col4:
+        albumin = st.text_input('Albumin')
+
+    with col5:
+        sugar = st.text_input('Sugar')
+
+    with col1:
+        red_blood_cells = st.text_input('Red Blood Cell')
+
+    with col2:
+        pus_cell = st.text_input('Pus Cell')
+
+    with col3:
+        pus_cell_clumps = st.text_input('Pus Cell Clumps')
+
+    with col4:
+        bacteria = st.text_input('Bacteria')
+
+    with col5:
+        blood_glucose_random = st.text_input('Blood Glucose Random')
+
+    with col1:
+        blood_urea = st.text_input('Blood Urea')
+
+    with col2:
+        serum_creatinine = st.text_input('Serum Creatinine')
+
+    with col3:
+        sodium = st.text_input('Sodium')
+
+    with col4:
+        potassium = st.text_input('Potassium')
+
+    with col5:
+        haemoglobin = st.text_input('Haemoglobin')
+
+    with col1:
+        packed_cell_volume = st.text_input('Packet Cell Volume')
+
+    with col2:
+        white_blood_cell_count = st.text_input('White Blood Cell Count')
+
+    with col3:
+        red_blood_cell_count = st.text_input('Red Blood Cell Count')
+
+    with col4:
+        hypertension = st.text_input('Hypertension')
+
+    with col5:
+        diabetes_mellitus = st.text_input('Diabetes Mellitus')
+
+    with col1:
+        coronary_artery_disease = st.text_input('Coronary Artery Disease')
+
+    with col2:
+        appetite = st.text_input('Appetitte')
+
+    with col3:
+        peda_edema = st.text_input('Peda Edema')
+    with col4:
+        aanemia = st.text_input('Aanemia')
+
+    # code for Prediction
+    kindey_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Kidney's Test Result"):
+
+        user_input = [age, blood_pressure, specific_gravity, albumin, sugar,
+       red_blood_cells, pus_cell, pus_cell_clumps, bacteria,
+       blood_glucose_random, blood_urea, serum_creatinine, sodium,
+       potassium, haemoglobin, packed_cell_volume,
+       white_blood_cell_count, red_blood_cell_count, hypertension,
+       diabetes_mellitus, coronary_artery_disease, appetite,
+       peda_edema, aanemia]
+
+        user_input = [float(x) for x in user_input]
+
+        prediction = kidney_disease_model.predict([user_input])
+
+        if prediction[0] == 1:
+            kindey_diagnosis = "The person has Kidney's disease"
+        else:
+            kindey_diagnosis = "The person does not have Kidney's disease"
+    st.success(kindey_diagnosis)
+
+
+# Chatbot Page
+if selected == 'Chatbot':
+    st.title("Ask Chatbot!")
+
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
+
+    prompt = st.chat_input("Pass your prompt here")
+
+    if prompt:
+        st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role': 'user', 'content': prompt})
+
+        try:
+            qa_chain = create_qa_chain()
+            response = qa_chain.invoke({'query': prompt})
+
+            result = response["result"]
+            source_documents = response["source_documents"]
+            result_to_show = result + "\nSource Docs:\n" + str(source_documents)
+
+            st.chat_message('assistant').markdown(result_to_show)
+            st.session_state.messages.append({'role': 'assistant', 'content': result_to_show})
+
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
